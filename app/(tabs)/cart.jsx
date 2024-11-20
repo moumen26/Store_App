35ccab3865a8dfa047b1636eb56c8be1abc07750
moumen26 <moumenkhaldi26@gmail.com>
2024-11-20 09:12:17 +0100

    Cart Screen

--- app/(tabs)/cart.jsx
+++ app/(tabs)/cart.jsx
@@ -1,12 +1,164 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, ScrollView, TextInput } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CartOrderItem from "../../components/CartOrderItem";
+import { MagnifyingGlassIcon } from "react-native-heroicons/outline";
 
-const Cart = () => {
+const COLUMN_COUNT = 1;
+const DATA = [
+  {
+    id: "1",
+    OrderStoreName: "Hamza Alimentation",
+    OrderID: "CDR45HGJF",
+    OrderType: "Delivery",
+    OrderDeliveryAddress: "Home (Rue Yousfi ..)",
+    OrderDate: "May 09,2024 | 04:00 PM",
+    OrderStatus: "Delivered",
+    OrderSubTotal: "990.00",
+  },
+  {
+    id: "2",
+    OrderStoreName: "Hichem Detergent",
+    OrderID: "CDR45HGJF",
+    OrderType: "Delivery",
+    OrderDeliveryAddress: "Home (Rue Yousfi ..)",
+    OrderDate: "May 09,2024 | 04:00 PM",
+    OrderStatus: "Preparing your order",
+    OrderSubTotal: "990.00",
+  },
+  {
+    id: "3",
+    OrderStoreName: "Item 3",
+    OrderID: "CDR45HGJF",
+    OrderType: "Delivery",
+    OrderDeliveryAddress: "Home (Rue Yousfi ..)",
+    OrderDate: "May 09,2024 | 04:00 PM",
+    OrderStatus: "Order on the way to address",
+    OrderSubTotal: "990.00",
+  },
+  {
+    id: "4",
+    OrderStoreName: "Item 4",
+    OrderID: "CDR45HGJF",
+    OrderType: "Delivery",
+    OrderDeliveryAddress: "Home (Rue Yousfi ..)",
+    OrderDate: "May 09,2024 | 04:00 PM",
+    OrderStatus: "Delivered",
+    OrderSubTotal: "990.00",
+  },
+  {
+    id: "5",
+    OrderStoreName: "Item 5",
+    OrderID: "CDR45HGJF",
+    OrderType: "Delivery",
+    OrderDeliveryAddress: "Home (Rue Yousfi ..)",
+    OrderDate: "May 09,2024 | 04:00 PM",
+    OrderStatus: "Delivered",
+    OrderSubTotal: "990.00",
+  },
+  {
+    id: "6",
+    OrderStoreName: "Item 6",
+    OrderID: "CDR45HGJF",
+    OrderType: "Delivery",
+    OrderDeliveryAddress: "Home (Rue Yousfi ..)",
+    OrderDate: "May 09,2024 | 04:00 PM",
+    OrderStatus: "Delivered",
+    OrderSubTotal: "990.00",
+  },
+  {
+    id: "7",
+    OrderStoreName: "Item 7",
+    OrderID: "CDR45HGJF",
+    OrderType: "Delivery",
+    OrderDeliveryAddress: "Home (Rue Yousfi ..)",
+    OrderDate: "May 09,2024 | 04:00 PM",
+    OrderStatus: "Delivered",
+    OrderSubTotal: "990.00",
+  },
+];
+
+const cart = () => {
+  const renderItems = () => {
+    const items = [];
+
+    for (let i = 0; i < DATA.length; i += COLUMN_COUNT) {
+      const rowItems = DATA.slice(i, i + COLUMN_COUNT).map((item) => (
+        <CartOrderItem
+          key={item.id}
+          OrderStoreName={item.OrderStoreName}
+          OrderID={item.OrderID}
+          OrderType={item.OrderType}
+          OrderDeliveryAddress={item.OrderDeliveryAddress}
+          OrderDate={item.OrderDate}
+          OrderStatus={item.OrderStatus}
+          OrderSubTotal={item.OrderSubTotal}
+        />
+      ));
+      items.push(
+        <View className="mb-4" key={i} style={styles.row}>
+          {rowItems}
+        </View>
+      );
+    }
+
+    return items;
+  };
+
   return (
-    <View>
-      <Text>Cart</Text>
-    </View>
+    <SafeAreaView className="bg-white pt-5 pb-12 relative h-full">
+      <Text className="text-center mb-[20]" style={styles.titleScreen}>
+        My Orders
+      </Text>
+      <View
+        style={styles.searchBar}
+        className="flex-row mx-5 items-center space-x-2 mb-[20] rounded-3xl"
+      >
+        <MagnifyingGlassIcon size={20} color="#26667E" />
+        <TextInput
+          style={styles.searchBarItem}
+          placeholder="Search your store.."
+          placeholderTextColor="#888888"
+          // value={searchQuery}
+          // onChangeText={setSearchQuery}
+        />
+      </View>
+      <ScrollView
+        className="mx-5"
+        contentContainerStyle={styles.container}
+        showsVerticalScrollIndicator={false}
+      >
+        {renderItems()}
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
-export default Cart;
+const styles = StyleSheet.create({
+  titleCategory: {
+    fontSize: 18,
+    fontFamily: "Montserrat-Regular",
+  },
+  titleScreen: {
+    fontSize: 20,
+    fontFamily: "Montserrat-Regular",
+  },
+  searchBar: {
+    height: 50,
+    borderColor: "#26667E",
+    borderWidth: 1,
+    alignItems: "center",
+    paddingLeft: 15,
+    borderRadius: 30,
+    flexDirection: "row",
+    gap: 4,
+  },
+  searchBarItem: {
+    color: "black",
+    fontSize: 12,
+    fontFamily: "Montserrat-Regular",
+    width: 220,
+  },
+});
+
+export default cart;
---
+++ components/CartOrderItem.jsx
@@ -0,0 +1,90 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { ChevronRightIcon } from "react-native-heroicons/outline";
+
+const CartOrderItem = ({
+  OrderStoreName,
+  OrderID,
+  OrderType,
+  OrderDeliveryAddress,
+  OrderDate,
+  OrderStatus,
+  OrderSubTotal,
+}) => {
+  return (
+    <View style={styles.cartOrderItem}>
+      <View style={styles.cartItem}>
+        <Text style={styles.text}>Store</Text>
+        <Text style={styles.textDescription}>{OrderStoreName}</Text>
+      </View>
+      <View style={styles.cartItem}>
+        <Text style={styles.text}>Order ID</Text>
+        <Text style={styles.textDescription}>{OrderID}</Text>
+      </View>
+      <View style={styles.cartItem}>
+        <Text style={styles.text}>Order Type</Text>
+        <Text style={styles.textDescription}>{OrderType}</Text>
+      </View>
+      <View style={styles.cartItem}>
+        <Text style={styles.text}>Delivery Address</Text>
+        <Text style={styles.textDescription}>{OrderDeliveryAddress}</Text>
+      </View>
+      <View style={styles.cartItem}>
+        <Text style={styles.text}>Order Date</Text>
+        <Text style={styles.textDescription}>{OrderDate}</Text>
+      </View>
+      <View style={styles.cartItem}>
+        <Text style={styles.text}>Status</Text>
+        <Text style={styles.textDescription}>{OrderStatus}</Text>
+      </View>
+      <View style={styles.cartItem}>
+        <Text style={styles.text}>Sub Total</Text>
+        <Text style={styles.textDescription}>DA {OrderSubTotal}</Text>
+      </View>
+      <View
+        style={styles.cartItemMoreDetails}
+        className="flex-row justify-end pr-[15] w-full"
+      >
+        <TouchableOpacity className="flex-row items-center space-x-1 mt-2">
+          <Text style={styles.text}>More Detail</Text>
+          <ChevronRightIcon size={11} color="#888888" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 11,
+    color: "#888888",
+    fontFamily: "Montserrat-Regular",
+  },
+  textDescription: {
+    fontSize: 11,
+    fontFamily: "Montserrat-Medium",
+  },
+  cartOrderItem: {
+    height: 200,
+    borderColor: "#C9E4EE",
+    borderWidth: 1,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 6,
+  },
+  cartItem: {
+    width: "100%",
+    flexDirection: "row",
+    paddingHorizontal: 15,
+    justifyContent: "space-between",
+  },
+  cartItemMoreDetails: {
+    width: "100%",
+    justifyContent: "flex-end",
+    paddingHorizontal: 15,
+    marginTop: 5,
+  },
+});
+
+export default CartOrderItem;
